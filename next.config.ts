00299771ffc5cb9d35fb88017c1d328f4410405a
u_m1mkt <sowtame@gmail.com>
2024-11-27 15:35:13 +0700

    csp

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,34 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
+
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    frame-ancestors https://*.stepantest.ru;
+    upgrade-insecure-requests;
+`
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+        ],
+      },
+    ]
+  },
+}
 
-export default nextConfig;
+export default nextConfig
